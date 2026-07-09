0c495edfa067813a1f5a815037b5942ee3bccb17
hankyeongmoo <165135691+hankyeongmoo@users.noreply.github.com>
2026-07-09 20:17:28 +0900

    달리기(?)

--- Assets/03.Scripts/CameraControl.cs
+++ Assets/03.Scripts/CameraControl.cs
@@ -3,7 +3,11 @@ using UnityEngine;
 public class CameraControl : MonoBehaviour
 {
     public float moveSpeed = 5f;
+    public float runSpeed = 10f;
     public float rotationSpeed = 100f;
+    
+    private float appliedSpeed;
+
 
     void Update()
     {
@@ -16,29 +20,39 @@ public class CameraControl : MonoBehaviour
         // moving from WASD
         if (Input.GetKey(KeyCode.W))
         {
-            transform.Translate(Vector3.forward * moveSpeed * Time.deltaTime);
+            transform.Translate(Vector3.forward * appliedSpeed * Time.deltaTime);
         }
         if (Input.GetKey(KeyCode.S))
         {
-            transform.Translate(Vector3.back * moveSpeed * Time.deltaTime);
+            transform.Translate(Vector3.back * appliedSpeed * Time.deltaTime);
         }
         if (Input.GetKey(KeyCode.A))
         {
-            transform.Translate(Vector3.left * moveSpeed * Time.deltaTime);
+            transform.Translate(Vector3.left * appliedSpeed * Time.deltaTime);
         }
         if (Input.GetKey(KeyCode.D))
         {
-            transform.Translate(Vector3.right * moveSpeed * Time.deltaTime);
+            transform.Translate(Vector3.right * appliedSpeed * Time.deltaTime);
         }
 
         // Vertical movement from Space and Left Ctrl
         if (Input.GetKey(KeyCode.Space))
         {
-            transform.Translate(Vector3.up * moveSpeed * Time.deltaTime);
+            transform.Translate(Vector3.up * appliedSpeed * Time.deltaTime);
         }
         if (Input.GetKey(KeyCode.LeftControl))
         {
-            transform.Translate(Vector3.down * moveSpeed * Time.deltaTime);
+            transform.Translate(Vector3.down * appliedSpeed * Time.deltaTime);
+        }
+
+        // running with Left Shift
+        if (Input.GetKey(KeyCode.LeftShift))
+        {
+            appliedSpeed = runSpeed;
+        }
+        else
+        {
+            appliedSpeed = moveSpeed;
         }
     }
 
